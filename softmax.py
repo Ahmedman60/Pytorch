4368818ba99a6257cb2f1af8e8ccc59e180ac3bc
Ahmedman60 <mohamedx_x2015@yahoo.com>
2023-07-10 11:40:55 +0300

    First commit

---
+++ softmax.py
@@ -0,0 +1,9 @@
+
+while True:
+    user_input = input("Enter Your Name : ")
+    if user_input:
+        break
+    else:
+        print("Invalid input. Please try again.")
+
+print("You entered:", user_input)
