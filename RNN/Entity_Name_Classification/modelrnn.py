16bc8a0a774c893a849427ef79ebfd280edf6791
Mohamed Fathallah <32023899+Ahmedman60@users.noreply.github.com>
2024-11-21 15:42:07 +0200

    Fixing Issues with history
    Remove history of repo  and fixing RNN

---
+++ RNN/Entity_Name_Classification/modelrnn.py
@@ -0,0 +1,6 @@
+# Creating the Network
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from utils import all_letters, all_categories
+from utils import load_data, load_random_example, letterToTensor, lineToTensor
