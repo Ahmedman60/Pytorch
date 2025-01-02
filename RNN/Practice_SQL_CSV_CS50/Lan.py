208bd7753b9689d1d433a5ad3763bff1232626b7
Mohamed Fathallah <32023899+Ahmedman60@users.noreply.github.com>
2025-01-02 15:52:25 +0200

    Update Lan.py
    Flat  file and sorting with dict

--- RNN/Practice_SQL_CSV_CS50/Lan.py
+++ RNN/Practice_SQL_CSV_CS50/Lan.py
@@ -1,1 +1,33 @@
 import csv
+import os
+
+# # print(os.path.dirname())
+# # print(__file__)  # this gives you the full path to the file
+
+# with open(os.path.join(os.path.dirname(__file__), 'train.csv'), 'r') as f:
+#     reader = csv.reader(f)
+#     next(reader)  # skip the header
+#     for row in reader:
+#         print(row)
+
+
+# with open(os.path.join(os.path.dirname(__file__), 'train.csv'), 'r') as f:
+#     reader = csv.DictReader(f)
+#     for row in reader:
+#         print(row)
+#         print(row["score"])
+
+
+# Counting number of training examples for each score
+with open(os.path.join(os.path.dirname(__file__), 'train.csv'), 'r') as f:
+    reader = csv.DictReader(f)
+    count = {}
+    for row in reader:
+        if row["score"] in count:
+            count[row["score"]] += 1
+        else:
+            count[row["score"]] = 1
+
+
+for score, count in sorted(count.items()):
+    print(f"{score}: {count}")
