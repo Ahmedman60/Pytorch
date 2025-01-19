f679c5fc94c5224ea99a711e83d47d39efac10eb
Ahmedman60 <Mohamedx_x2015@yahoo.com>
2025-01-19 20:00:21 +0200

    Lab2 of Generative Ai
    the lab and data

--- Learning/Optimization/testinggananaconda.py
+++ Learning/Optimization/testinggananaconda.py
@@ -1,4 +1,5 @@
 import torch
+import seaborn as sns
 
 # Check if CUDA is available
 print("CUDA Available:", torch.cuda.is_available())
@@ -6,3 +7,6 @@ print("CUDA Available:", torch.cuda.is_available())
 # Get the current GPU device
 if torch.cuda.is_available():
     print("CUDA Device Name:", torch.cuda.get_device_name(0))
+
+
+# YOU NEED TO INSTALL all the other modules for machine learning
