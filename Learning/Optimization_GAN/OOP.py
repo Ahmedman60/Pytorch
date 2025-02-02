8e59e281fc29cc0aa095fd8f267a83bab0b6abf9
Ahmedman60 <Mohamedx_x2015@yahoo.com>
2025-02-02 13:17:11 +0200

    Enhancements

--- Learning/Optimization_GAN/OOP.py
+++ Learning/Optimization_GAN/OOP.py
@@ -6,6 +6,7 @@ class Website:
     def __init__(self, url):
         self.status = None
         self.url = url
+        self.z = 20
 
     def get_status(self):
         if self.url:  # Ensure URL is not None
@@ -24,10 +25,42 @@ class Website:
             # This means the status didn't change
             return False
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.url}) ,{self.__dict__} "
+
 
 youtube = Website("https://google.com")
 
-for _ in range(5):
-    time.sleep(1)
-    print(youtube.get_status())
-    print(youtube.is_changed())
+# for _ in range(5):
+#     print(youtube.get_status())
+#     print(youtube.is_changed())
+
+
+# This is same as  youtube.z
+print(getattr(youtube, "z"))
+
+# if the attribute isn't exist you can get default value
+print(getattr(youtube, "z", 10))
+
+
+# set attribute or delattribute is to create or delete attributes of objects in runtime
+setattr(youtube, "z", 30)
+print(youtube.z)
+
+delattr(youtube, "z")  # delete attributes of objects
+# print(youtube.z)  # AttributeError: 'Website' object has no attribute 'z'
+
+
+# we can use dictionary of attributes
+
+website_dict = {
+    "users": 20,
+    "videos": 100
+}
+
+for k, v in website_dict.items():
+    setattr(youtube, k, v)
+    print(f"{k}: {getattr(youtube, k)}")
+
+
+print(youtube)
