a502697f1ca966d89df6dd862df536d86937b695
Ahmedman60 <Mohamedx_x2015@yahoo.com>
2025-03-27 05:53:58 +0200

    Update Longest Subarray of 1's After Deleting One Element.py
    Different solutions

--- Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
+++ Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
@@ -1,29 +1,29 @@
-# class Solution(object):
-#     def longestSubarray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         if nums.count(1) == len(nums):
-#             return len(nums)-1
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums.count(1) == len(nums):
+            return len(nums)-1
 
-#         max_len = 0
-#         left = 0
-#         delete = False
-#         for right in range(len(nums)):
-#             if delete:
-#                 left += 1
-#                 delete = False
+        max_len = 0
+        left = 0
+        delete = False
+        for right in range(len(nums)):
+            if delete:
+                left += 1
+                delete = False
 
-#             if nums[right] == 0:
-#                 delete = True
+            if nums[right] == 0:
+                delete = True
 
-#             if delete:
-#                 max_len = max(max_len, (right-left+1)-1)
-#             else:
-#                 max_len = max(max_len, (right-left+1))
+            if delete:
+                max_len = max(max_len, (right-left+1)-1)
+            else:
+                max_len = max(max_len, (right-left+1))
 
-#         return max_len
+        return max_len
 
 
 # print(Solution().longestSubarray([1, 1, 1]))  # 3
@@ -60,25 +60,78 @@
 #             max_len = max(max_len, current_len)
 
 
-class Solution(object):
-    def longestSubarray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        left = 0
-        zero_index = -1
-        max_len = 0
-        # [0, 1, 1, 1, 0, 1, 1, 0, 1]
-        for right in range(len(nums)):
+# class Solution(object):
+#     def longestSubarray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         left = 0
+#         zero_index = -1
+#         max_len = 0
+#         # [0, 1, 1, 1, 0, 1, 1, 0, 1]
+#         for right in range(len(nums)):
 
-            if nums[right] == 0:
-                left = zero_index + 1
-                zero_index = right
+#             if nums[right] == 0:
+#                 left = zero_index + 1
+#                 zero_index = right
 
-            max_len = max(max_len, right - left)
+#             max_len = max(max_len, right - left)
 
-        return max_len
+#         return max_len
 
 
-print(Solution().longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]))  # 3
+# print(Solution().longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]))  # 3
+
+
+# class Solution(object):
+#     def longestSubarray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         left = 0
+#         zero_count = 0
+#         max_len = 0
+
+#         for right in range(len(nums)):
+
+#             if nums[right] == 0:
+#                 zero_count += 1
+
+#             while zero_count > 1:
+#                 if nums[left] == 0:
+#                     zero_count -= 1
+#                 left += 1
+
+#             # Update max_len (subtracting one because we must delete one element)
+#             max_len = max(max_len, right - left)
+
+#         return max_len
+
+
+# class Solution(object):
+#     def longestSubarray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         left = 0
+#         zero_count = 0
+#         max_len = 0
+
+#         for right in range(len(nums)):
+
+#             zero_count += 1 - nums[right]  # 1-1=0 1-0=1
+
+#             while zero_count > 1:
+
+#                 zero_count -= 1-nums[left]
+#                 # if nums[left] == 0:
+#                 #     zero_count -= 1
+#                 left += 1
+
+#             # Update max_len (subtracting one because we must delete one element)
+#             max_len = max(max_len, right - left)
+
+#         return max_len
