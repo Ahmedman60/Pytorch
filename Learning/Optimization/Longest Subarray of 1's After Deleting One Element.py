54777b4655daf5b0fee2045a32cc296c4f93a18d
Ahmedman60 <Mohamedx_x2015@yahoo.com>
2025-03-27 05:39:22 +0200

    Update Longest Subarray of 1's After Deleting One Element.py
    Updates

--- Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
+++ Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
@@ -67,22 +67,15 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        zero_count = 0
+        zero_index = -1
         max_len = 0
-
+        # [0, 1, 1, 1, 0, 1, 1, 0, 1]
         for right in range(len(nums)):
 
             if nums[right] == 0:
-                zero_count += 1
+                left = zero_index + 1
+                zero_index = right
 
-            while zero_count > 1:
-
-                if nums[left] == 0:
-                    zero_count -= 1
-
-                left += 1
-
-            # Update max_len (subtracting one because we must delete one element)
             max_len = max(max_len, right - left)
 
         return max_len
