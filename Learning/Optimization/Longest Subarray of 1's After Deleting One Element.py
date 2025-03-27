c4ab830c8ca23996df9f94ee2bf14c6528d5da89
Ahmedman60 <Mohamedx_x2015@yahoo.com>
2025-03-27 23:59:15 +0200

    leet code shit
    Sliding windows again and again and again

--- Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
+++ Learning/Optimization/Longest Subarray of 1's After Deleting One Element.py
@@ -1,137 +1,58 @@
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         # pwwkew
+#         seen = set()
+#         left = 0
+#         max_lenght = 0
+#         for right in range(len(s)):
+
+#             if s[right] not in seen:
+#                 seen.add(s[right])
+#             else:
+#                 # remove eveything and start new substring
+#                 while s[right] in seen:
+#                     seen.remove(s[left])
+#                     left += 1
+
+#             seen.add(s[right])
+#             max_lenght = max(max_lenght, (right-left)+1)
+
+#         return max_lenght
+
+
+# s = Solution()
+# print(s.lengthOfLongestSubstring("dvdf"))
+
+
 class Solution(object):
-    def longestSubarray(self, nums):
+    def lengthOfLongestSubstring(self, s):
         """
-        :type nums: List[int]
+        :type s: str
         :rtype: int
         """
-        if nums.count(1) == len(nums):
-            return len(nums)-1
-
-        max_len = 0
+        seen = set()
         left = 0
-        delete = False
-        for right in range(len(nums)):
-            if delete:
-                left += 1
-                delete = False
+        max_length = 0
 
-            if nums[right] == 0:
-                delete = True
-
-            if delete:
-                max_len = max(max_len, (right-left+1)-1)
+        for right in range(len(s)):
+            # only remove characters until you remove the first occurrence of char.
+            if s[right] not in seen:
+                seen.add(s[right])
             else:
-                max_len = max(max_len, (right-left+1))
+                while s[right] in seen:
+                    seen.remove(s[left])
+                    left += 1
+                seen.add(s[right])
 
-        return max_len
+            max_length = max(max_length, right - left + 1)
 
-
-# print(Solution().longestSubarray([1, 1, 1]))  # 3
-
-
-# class Solution(object):
-#     def longestSubarray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         if nums.count(1) == len(nums):
-#             return len(nums)-1
-
-#         left = 0
-#         current_len = 0
-#         delete = False
-#         max_len = 0
-#         for right in range(len(nums)):
-
-#             if nums[right] == 0:
-#                 delete = True
-#                 # for ones
-#                 while nums[left] == 1:
-#                     left += 1
-#                 # for the zero
-#                 left += 1
-#             if delete == True:
-#                 current_len += right-left+1
-#             else:
-#                 current_len = right-left+1
-
-#             delete = False
-#             max_len = max(max_len, current_len)
+        return max_length
 
 
-# class Solution(object):
-#     def longestSubarray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         left = 0
-#         zero_index = -1
-#         max_len = 0
-#         # [0, 1, 1, 1, 0, 1, 1, 0, 1]
-#         for right in range(len(nums)):
+s = Solution().lengthOfLongestSubstring("abccccba")
 
-#             if nums[right] == 0:
-#                 left = zero_index + 1
-#                 zero_index = right
-
-#             max_len = max(max_len, right - left)
-
-#         return max_len
-
-
-# print(Solution().longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]))  # 3
-
-
-# class Solution(object):
-#     def longestSubarray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         left = 0
-#         zero_count = 0
-#         max_len = 0
-
-#         for right in range(len(nums)):
-
-#             if nums[right] == 0:
-#                 zero_count += 1
-
-#             while zero_count > 1:
-#                 if nums[left] == 0:
-#                     zero_count -= 1
-#                 left += 1
-
-#             # Update max_len (subtracting one because we must delete one element)
-#             max_len = max(max_len, right - left)
-
-#         return max_len
-
-
-# class Solution(object):
-#     def longestSubarray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         left = 0
-#         zero_count = 0
-#         max_len = 0
-
-#         for right in range(len(nums)):
-
-#             zero_count += 1 - nums[right]  # 1-1=0 1-0=1
-
-#             while zero_count > 1:
-
-#                 zero_count -= 1-nums[left]
-#                 # if nums[left] == 0:
-#                 #     zero_count -= 1
-#                 left += 1
-
-#             # Update max_len (subtracting one because we must delete one element)
-#             max_len = max(max_len, right - left)
-
-#         return max_len
+print(s)
